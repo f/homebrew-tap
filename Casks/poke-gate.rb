7bf359af8bb0a9e377a9a1e4d2181681089938d6
Fatih Kadir Akın <fatihkadirakin@gmail.com>
2026-03-24 00:39:59 +0300

    Update poke-gate.rb

--- Casks/poke-gate.rb
+++ Casks/poke-gate.rb
@@ -2,7 +2,7 @@ cask "poke-gate" do
   version "0.0.4"
   sha256 "6a0eca60c1fc1bc66f2a1e65005fef96a3d5e8c1ebd9b211854463ee4801a768"
 
-  url "https://github.com/f/poke-gate/releases/download/v0.0.4/Poke%20macOS%20Gate.dmg"
+  url "https://github.com/f/poke-gate/releases/download/v0.0.4/Poke.macOS.Gate.dmg"
   name "Poke Gate"
   desc "macOS menu bar app to expose your machine to your Poke AI assistant"
   homepage "https://github.com/f/poke-gate"
